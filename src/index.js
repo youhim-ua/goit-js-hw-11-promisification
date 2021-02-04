ed105b0533be2711178997f887816b4578b6ddf9
Andrew Chepur-Krytskyi <66245509+youhim-ua@users.noreply.github.com>
2021-02-04 23:10:53 +0200

    add promise

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+import './js/delay-1.js';
+import './js/toggle-users-2.js';
+import './js/transaction-3.js';
---
+++ src/js/toggle-users-2.js
@@ -0,0 +1,25 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => 
+  new Promise(resolve =>
+    resolve(allUsers.map(user => user.name === userName ? { ...user, active: !user.active } : user))
+  );
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+* Сейчас работает так
+*/
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+/*
+* Должно работать так
+*/
+toggleUserState(users, 'Mango').then(logger);
+
+toggleUserState(users, 'Lux').then(logger);
